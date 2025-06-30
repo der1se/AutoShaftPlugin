0fdff642bbfd9feed81d9abe6dd09af8ab08d23c
der1se <markdruz@icloud.com>
2025-06-30 21:55:25 +0300

    AutoShaftPlugin.java

--- src/main/java/com/example/autoshaft/AutoShaftPlugin.java
+++ src/main/java/com/example/autoshaft/AutoShaftPlugin.java
@@ -1,1 +1,25 @@
+package com.example.autoshaft;
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class AutoShaftPlugin extends JavaPlugin {
+
+    private Map<String, Mine> mines = new HashMap<>();
+
+    @Override
+    public void onEnable() {
